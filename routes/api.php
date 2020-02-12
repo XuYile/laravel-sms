64bc6909375e9c5ace4fcb6e65c97a2c6e27b0c9
xyc <xyc@winseeing.cn>
2020-02-12 06:16:30 +0000

    frist commit

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::prefix('v1')->name('api.v1')->group(function (){
+    Route::get('version',function (){
+        return "This is version v1";
+    })->name('version');
+});
+

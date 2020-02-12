6a852b46988387baf30aa4c0b057d643b40f425d
XuYile <15265232081@163.com>
2020-02-12 16:37:58 +0800

    短信发送基础1

--- app/Providers/EasySmsServiceProvider.php
+++ app/Providers/EasySmsServiceProvider.php
@@ -2,27 +2,22 @@
 
 namespace App\Providers;
 
+use Overtrue\EasySms\EasySms;
 use Illuminate\Support\ServiceProvider;
 
 class EasySmsServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
+    public function boot()
     {
         //
     }
 
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function register()
     {
-        //
+        $this->app->singleton(EasySms::class, function ($app) {
+            return new EasySms(config('easysms'));
+        });
+
+        $this->app->alias(EasySms::class, 'easysms');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,10 @@ Route::prefix('v1')->name('api.v1')->group(function (){
     Route::get('version',function (){
         return "This is version v1";
     })->name('version');
+
+    // 用户注册
+    Route::post('users', 'UsersController@store')
+        ->name('users.store');
+
 });
 
